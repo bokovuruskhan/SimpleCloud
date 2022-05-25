d5c1d1b7533ee87493e84673c94bcd32d45749ac
Uruskhan Bokov <bokovuruskhan@gmail.com>
2022-05-25 16:34:17 +0300

    add service annotation

--- src/main/java/com/simple/cloud/service/NoteService.java
+++ src/main/java/com/simple/cloud/service/NoteService.java
@@ -3,7 +3,9 @@ package com.simple.cloud.service;
 import com.simple.cloud.data.model.Note;
 import com.simple.cloud.data.repo.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class NoteService {
 
     @Autowired
